38d06475322e26aff4aa1ba668f5946e39059cee
Michael Treser <armikael2@gmail.com>
2018-08-15 17:50:33 +0300

    Updated isInArray function to check strings array.

--- lesson-2/TreserMichael/functions.ts
+++ lesson-2/TreserMichael/functions.ts
@@ -7,7 +7,7 @@
         if (args) {
             args.forEach(argument => {
                 if (arr.indexOf(argument) === -1) {
-                    console.log(`This argument ${argument} doesn't exist in provided array.`);
+                    console.log(`This argument - ${argument} doesn't exist in provided array.`);
                     return false;    
                 }
 
@@ -19,13 +19,18 @@
         return false;
     }
 
-    let numbersList = [2, 4, 6, 8, 10];
+    let numbersList: number[] = [2, 4, 6, 8, 10];
+    let stringsList: string[] = ['May', 'June', 'August'];
 
-    let result1 = isInArray(numbersList); // No arguments
-    let result2 = isInArray(numbersList, 2, 4); // Two existing arguments
-    let result3 = isInArray(numbersList, 2, 4, 7); // Three arguments with one that's not in array
+    let result1: boolean = isInArray(numbersList); // No arguments
+    let result2: boolean = isInArray(numbersList, 2, 4); // Two existing arguments
+    let result3: boolean = isInArray(numbersList, 2, 4, 7); // Three arguments with 1 that's not in array
+    let result4: boolean = isInArray(stringsList, 'May'); // Strings array with 1 argument
+    let result5: boolean = isInArray(stringsList, 'June', 'October'); // Strings array with 1 arguments that's not in array
 
     console.log('No arguments: ', result1);
     console.log('Two existing arguments: ', result2);
     console.log('Three arguments with one that\'s not in array: ', result3);
+    console.log('Strings array with 1 argument: ', result4);
+    console.log('Strings array with 1 arguments that\'s not in array: ', result5);
 })();
